41a29ea4a39d6c5076ee0eabfd539e153f48d6c2
Łukasz Czerwiński <mrowacz@gmail.com>
2017-10-02 23:21:41 +0200

    sql: add db scheme
    Add tables
    - doctor
    - patient
    - examination
    - visit
    
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

--- database.sql
+++ database.sql
@@ -1,20 +1,60 @@
+drop database clinic;
 create database if not exists clinic;
-use clinic;
 
-drop table patients;
+use clinic;
+drop table if exists patients;
+drop table if exists doctors;
+drop table if exists visits;
+drop table if exists examinations;
 
 create table  patients (
-    id int,
+    id int not null,
     name varchar(100),
     address varchar(50),
     pesel varchar(12),
-    phone varchar (12)
+    phone varchar (12),
+    
+    primary key(id)
 );
 
+create table doctors (
+    id int not null,
+    specialization int,
+    name varchar(100),
+    address varchar(50),
+    pesel varchar(12),
+    phone varchar (12),
+    salary real,
+    
+    primary key(id)
+);
 
+create table visits (
+    id int not null,
+    dt date,
+    doctor int,
+    patient int,
+    disease varchar(100),
+    medicaments text,
+    examinations text,
+    
+    primary key(id),
+    foreign key(doctor)
+        references doctors(id)
+);
 
-
-
+create table examinations (
+    id int not null,
+    extype text,
+    description text,
+    doc_opinion text,
+    patient int,
+    doctor int,
+    done boolean,
+    commented boolean,
+    
+    primary key(id)
+);
 
 
 
