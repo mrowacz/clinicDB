e430f72a9e1e4a28a115e4567b13a6c8fcb6f31e
Łukasz Czerwiński <mrowacz@gmail.com>
2017-10-04 23:46:23 +0200

    db: update sql + add python generator
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

--- database.sql
+++ database.sql
@@ -1,35 +1,39 @@
 drop database clinic;
 create database if not exists clinic;
-
 use clinic;
-drop table if exists patients;
-drop table if exists doctors;
-drop table if exists visits;
 
 create table  patients (
-    id int not null,
+    id int not null auto_increment,
     name varchar(100),
-    address varchar(50),
+    address varchar(100),
     pesel varchar(12),
-    phone varchar (12),
+    phone varchar(25),
     
+    primary key(id)
+);
+
+create table specializations (
+    id int not null auto_increment,
+    spec text,
     primary key(id)
 );
 
 create table doctors (
-    id int not null,
-    specialization int,
+    id int not null auto_increment,
     name varchar(100),
-    address varchar(50),
+    address varchar(100),
     pesel varchar(12),
-    phone varchar (12),
+    phone varchar(25),
+    specialization int,
     salary real,
     
-    primary key(id)
+    primary key(id),
+    foreign key(specialization)
+        references specializations(id)
 );
 
 create table visits (
-    id int not null,
+    id int not null auto_increment,
     dt date,
     doctor int,
     patient int,
--- fill_db.py
+++ fill_db.py
@@ -1,18 +1,41 @@
+import random
 import mysql.connector
 from faker import Factory
+
+# config
+patients = 1000;
+doctors = 200;
 
 cnx = mysql.connector.connect(user='root', database='clinic', password='db-password')
 cur = cnx.cursor(buffered=True)
 
+# queries
 new_patient = (
     "INSERT INTO patients (name, address, phone) "
     "VALUES (%s, %s, %s)"
-  )
+)
 
-for x in xrange(100):
+new_doctor = (
+    "INSERT INTO doctors (name, address, phone, specialization, salary) "
+    "VALUES (%s, %s, %s, %s, %s)"
+)
+
+for x in xrange(patients):
     fake = Factory.create()
     cur.execute(new_patient, (fake.name(), fake.address(), fake.phone_number()))
     cnx.commit()
+
+specializations = map(lambda x: x.rstrip(), open("specialty_list.txt").readlines())
+for spec in specializations:
+    cur.execute("INSERT INTO specializations(spec) VALUES (%s)", (spec,))
+cnx.commit()
+
+for x in xrange(doctors):
+     fake = Factory.create()
+     cur.execute(new_doctor,
+         (fake.name(), fake.address(), fake.phone_number(), random.randint(1, len(specializations)),
+         random.randint(6000, 20000)))
+     cnx.commit()
 
 cnx.close()
 
